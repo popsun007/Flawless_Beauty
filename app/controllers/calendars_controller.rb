bd4fb553651b6c188570086896c6e5dd96923258
Frank Sun <popsun2010@gmail.com>
2017-07-28 21:41:41 -0700

    Added OAuth2 authentication to Calendars controller

---
+++ app/controllers/calendars_controller.rb
@@ -0,0 +1,33 @@
+class CalendarsController < ApplicationController
+  def index
+    render :layout => true
+  end
+
+  def redirect
+    client = Signet::OAuth2::Client.new({
+      client_id: Rails.application.secrets.google_client_id,
+      client_secret: Rails.application.secrets.google_client_secret,
+      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
+      redirect_uri: callback_url
+    })
+
+    redirect_to client.authorization_uri.to_s
+  end
+
+  def callback
+    client = Signet::OAuth2::Client.new({
+      client_id: Rails.application.secrets.google_client_id,
+      client_secret: Rails.application.secrets.google_client_secret,
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      redirect_uri: callback_url,
+      code: params[:code]
+    })
+
+    response = client.fetch_access_token!
+
+    session[:authorization] = response
+
+    redirect_to calendars_url
+  end
+end
