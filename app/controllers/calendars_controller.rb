427a992cc1c8c000b7ea78c2f19a51892dbd466a
Frank Sun <popsun2010@gmail.com>
2017-07-30 21:37:32 -0700

    Parsed the date and time data, fixed the Google OAuth2 issue

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,51 +1,26 @@
 class CalendarsController < ApplicationController
+  before_action :authenticate_user!
   skip_before_filter  :verify_authenticity_token #Allow making POST request to Google API
 
   def index
+    render :layout => true
+  end
+
+  def callback
     client = Signet::OAuth2::Client.new({
       client_id: Rails.application.secrets.google_client_id,
       client_secret: Rails.application.secrets.google_client_secret,
-      token_credential_uri: 'https://accounts.google.com/o/oauth2/token'
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      redirect_uri: callback_url,
+      code: params[:code]
     })
 
-    client.update!(session[:authorization])
+    response = client.fetch_access_token!
 
-    service = Google::Apis::CalendarV3::CalendarService.new
-    service.authorization = client
+    session[:authorization] = response
 
-    # @event_list = service.list_calendar_lists
-    @event_list = service.list_events("flawlessbeautyfremont@gmail.com")
-    # render :layout => true
+    redirect_to "/"
   end
-
-  # def redirect
-  #   client = Signet::OAuth2::Client.new({
-  #     client_id: Rails.application.secrets.google_client_id,
-  #     client_secret: Rails.application.secrets.google_client_secret,
-  #     authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
-  #     scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
-  #     redirect_uri: callback_url
-  #   })
-
-  #   redirect_to client.authorization_uri.to_s
-  # end
-
-  # def callback
-  #   client = Signet::OAuth2::Client.new({
-  #     client_id: Rails.application.secrets.google_client_id,
-  #     client_secret: Rails.application.secrets.google_client_secret,
-  #     token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
-  #     redirect_uri: callback_url,
-  #     code: params[:code]
-  #   })
-
-  #   response = client.fetch_access_token!
-
-  #   session[:authorization] = response
-
-  #   redirect_to calendars_url
-  # end
-
 
   def booking
     Reservation.reservation_confirmation(params).deliver
@@ -62,27 +37,47 @@ class CalendarsController < ApplicationController
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = client
 
-    start_time = Date.strptime(appointment_params["reservation-date"], '%m/%d/%Y').strftime('%Y-%m-%d')
-    start_time = Date.parse start_time
+    #Parse the date and time 
+    date = Date.strptime(appointment_params["reservation-date"], '%m/%d/%Y').strftime('%Y-%m-%d')
+    date = Date.parse(date)
+    start_time = Time.parse(appointment_params["reservation-time"])
+    duration_arr = appointment_params["reservation-duration"].split(".")
+    end_time = start_time + duration_arr.first.to_i.hours + duration_arr.last.to_i.minutes
+
+    start_time = DateTime.new(date.year, date.month, date.day, start_time.hour, start_time.min, start_time.sec, start_time.zone)
+    end_time = DateTime.new(date.year, date.month, date.day, end_time.hour, end_time.min, end_time.sec, end_time.zone)
+
+    start_time = start_time.to_datetime.rfc3339
+    end_time = end_time.to_datetime.rfc3339
 
     event = Google::Apis::CalendarV3::Event.new({
-      start: Google::Apis::CalendarV3::EventDateTime.new(date: start_time),
-      end: Google::Apis::CalendarV3::EventDateTime.new(date: start_time + appointment_params["reservation-duration"].to_f),
-      summary: 'Event created from rails!'
+      start: Google::Apis::CalendarV3::EventDateTime.new(date_time: start_time),
+      end: Google::Apis::CalendarV3::EventDateTime.new(date_time: end_time),
+      summary: appointment_params["reservation-name"] + " " + appointment_params["reservation-phone"],
+      description: "Email: " + appointment_params["reservation-email"] + "\n" + appointment_params["reservation-note"]
     })
 
-    begin 
-      service.insert_event("flawlessbeautyfremont@gmail.com", event)
-    rescue Google::Apis::AuthorizationError => exception
-      client.authorization.grant_type = 'refresh_token'
-      response = client.refresh!
-
-      session[:authorization] = session[:authorization].merge(response)
-
-      retry
+    if session[:authorization].nil?
+      client = Signet::OAuth2::Client.new({
+        client_id: Rails.application.secrets.google_client_id,
+        client_secret: Rails.application.secrets.google_client_secret,
+        authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+        scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
+        redirect_uri: callback_url
+      })
+      redirect_to client.authorization_uri.to_s
+    else
+      begin 
+        service.insert_event("flawlessbeautyfremont@gmail.com", event)
+      rescue Google::Apis::AuthorizationError => exception
+          response = client.refresh!
+        
+          session[:authorization] = session[:authorization].merge(response)
+        retry
+      end
+      redirect_to "/calendars"
     end
-
-    redirect_to "/calendars"
+    
   end
 
 
