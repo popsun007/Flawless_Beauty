2f932d29224dae4901e8955f05456443d275a332
Frank Sun <popsun2010@gmail.com>
2017-11-12 19:23:24 -0800

    Fix migration issue

--- db/migrate/20171101024159_add_birthday_to_users.rb
+++ db/migrate/20171101024159_add_birthday_to_users.rb
@@ -1,5 +1,5 @@
 class AddBirthdayToUsers < ActiveRecord::Migration
   def change
-    change_column :users, :birthday, :date, default: Date.today
+    add_column :users, :birthday, :date, default: Date.today
   end
 end
